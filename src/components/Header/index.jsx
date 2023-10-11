35b25139c015d7a3fc3c3ed4d0b17f6334a90251
Portarriaux <jhonatacaetano14@hotmail.com>
2023-10-11 11:20:52 -0300

    Logout da aplicação.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,16 @@
 import { RiShutDownLine } from 'react-icons/ri'
 import { Container, Profile, Logout } from './styles'
+import { useAuth } from "../../hooks/auth"
 
 export function Header() {
+  const { signOut } = useAuth()
+
   return (
     <Container>
       <Profile to="/profile">
         <img
-          src="https://github.com/Portarriaux"
-          alt="Foto do usuário"
+           src="https://github.com/Portarriaux.png"
+           alt="Foto do usuário"
         />
 
         <div>
@@ -16,7 +19,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout>
+      <Logout onClick={ signOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -25,6 +25,13 @@ function AuthProvider({ children }) {
     }
   }
 
+  function signOut() {
+    const token = localStorage.removeItem("@rocketnotes:token");
+    const user = localStorage.removeItem("@rocketnotes:user");
+
+    setData({});
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@rocketnotes:token");
     const user = localStorage.getItem("@rocketnotes:user");
@@ -40,7 +47,13 @@ function AuthProvider({ children }) {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
+    <AuthContext.Provider
+      value={{
+        signIn,
+        signOut,
+        user: data.user,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );

c0f9e0d4a6588ca117d3451956a2650b3a264316
Portarriaux <jhonatacaetano14@hotmail.com>
2023-10-11 11:52:07 -0300

    Ajuste no header das requisições

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -14,7 +14,8 @@ function AuthProvider({ children }) {
       localStorage.setItem("@rocketnotes:user", JSON.stringify(user));
       localStorage.setItem("@rocketnotes:token", token);
 
-      api.defaults.headers.authorization = `Bearer ${token}`;
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
       setData({ user, token });
     } catch (error) {
       if (error.response) {

950239c0a1f409ceb0b1b9bfc64a65a542d26910
Portarriaux <jhonatacaetano14@hotmail.com>
2023-10-24 16:58:39 -0300

    Selecionando Tag

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
-
+import { useState, useEffect } from 'react'
+import { api } from "../../services/api"
 import { Note } from '../../components/Note'
 import { Input } from '../../components/Input'
 import { Header } from '../../components/Header'
@@ -8,6 +9,34 @@ import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
+  const [tags, setTags] = useState([])
+  const [tagsSelected, setTagsSelected] = useState([])
+
+  function handleTagSelected(tagName){
+    const alreadySelected = tagsSelected.includes(tagName)
+
+    if(alreadySelected) {
+      const filteredTags = tagsSelected.filter(tag => tag !== tagName)
+      setTagsSelected(filteredTags)
+    } else {
+      
+      setTagsSelected(prevState => [...prevState, tagName])
+    }
+
+
+  }
+
+  useEffect(() => {
+
+    async function fetchTags(){
+      const response = await api.get("/tags")
+      setTags(response.data)
+    }
+
+    fetchTags()
+
+  }, []) 
+
   return (
     <Container>
       <Brand>
@@ -17,9 +46,28 @@ export function Home() {
       <Header />
 
       <Menu>
-        <li><ButtonText title="Todos" $isactive /></li>
-        <li><ButtonText title="React" /></li>
-        <li><ButtonText title="Nodejs" /></li>
+        <li>
+          <ButtonText 
+            title="Todos"
+            onClick={() => handleTagSelected("all")}
+            $isactive={tagsSelected.length === 0 }
+           />
+        </li>
+
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+
+              <ButtonText 
+                title={tag.name}
+                onClick={() => handleTagSelected(tag.name)}
+                $isactive={tagsSelected.includes(tag.name)}
+              />
+
+            </li>
+          ))
+        }
+     
       </Menu>
 
       <Search>

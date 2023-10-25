c187406c3a62e7ff8b77e6cd4e4d05a594f5ae96
Portarriaux <jhonatacaetano14@hotmail.com>
2023-10-25 10:48:56 -0300

    Remover nota.

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -18,6 +18,15 @@ export function Details() {
     navigate("/")
   }
 
+  async function handleRemove (){
+    const confirm = window.confirm("Deseja realmente remover a nota?")
+
+    if(confirm) {
+      await api.delete(`/notes/${params.id}`)
+      navigate("/")
+    }
+  }
+
   useEffect(() => {
     async function fecthNote() {
       const response = await api.get(`/notes/${params.id}`);
@@ -35,7 +44,10 @@ export function Details() {
       {data && (
         <main>
           <Content>
-            <ButtonText title="Excluir nota" />
+            <ButtonText 
+              title="Excluir nota" 
+              onClick={handleRemove}
+            />
 
             <h1>{data.title}</h1>
 
